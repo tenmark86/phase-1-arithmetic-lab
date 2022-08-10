644eb5ca4c6be77c1b453b53a54e7f956c011b1a
tenmark86e <tenmark86@gmail.com>
2022-08-10 13:54:34 +0300

    Done Arithmetic Lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+
+/*Multiply */
+let num1=31;
+let num2=2;
+
+let multiply;
+multiply = num1*num2;
+multiply=62;
+
+/* Random number */
+let random=Math.floor(Math.random()*1000+1)
+
+/*Mod*/
+
+let num3 =10;
+let num4 = 6;
+let mod = num3 % num4;
+
+/*Max value */
+let  max=Math.max(3,6,18,20,9);
